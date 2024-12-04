f5e3320db8e3266a4cf7273f0e365b264e0666fe
Yang Bingdong <yangbingdong1994@gmail.com>
2024-12-05 00:27:24 +0800

    add compatible module to showcase multi jdk support

---
+++ compatible/base64/src/main/java/com/yangbingdong/java/newfeat/introduction/Base64Util.java
@@ -0,0 +1,32 @@
+package com.yangbingdong.java.newfeat.introduction;
+
+import java.lang.reflect.Method;
+import java.util.Base64;
+
+/**
+ * @author yangbingdong1994@gmail.com
+ */
+public class Base64Util {
+
+    public static String encode(String input) {
+        String encodedString = null;
+
+        try {
+            String version = System.getProperty("java.version");
+            if (version.startsWith("1.8")) {
+                System.out.println("Using Java8 BASE64Encoder");
+                Class<?> base64EncoderClass = Class.forName("sun.misc.BASE64Encoder");
+                Object base64Encoder = base64EncoderClass.newInstance();
+                Method encodeMethod = base64EncoderClass.getMethod("encode", byte[].class);
+                encodedString = (String) encodeMethod.invoke(base64Encoder, input.getBytes());
+            } else {
+                System.out.println("Using Java9+ Base64");
+                encodedString = Base64.getEncoder().encodeToString(input.getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return encodedString;
+    }
+}
---
+++ compatible/project-jdk21/src/main/java/com/yangbingdong/java/newfeat/introduction/Main21.java
@@ -0,0 +1,13 @@
+package com.yangbingdong.java.newfeat.introduction;
+
+/**
+ * @author yangbingdong1994@gmail.com
+ */
+public class Main21 {
+
+    public static void main(String[] args) {
+        String originalString = "Hello, Java!";
+        String encodedString = Base64Util.encode(originalString);
+        System.out.println("Encoded String: " + encodedString);
+    }
+}
