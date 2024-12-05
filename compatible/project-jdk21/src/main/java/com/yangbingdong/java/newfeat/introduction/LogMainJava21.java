22826243cc9b502d6dc206881b445362ef7c6dcc
Yang Bingdong <yangbingdong1994@gmail.com>
2024-12-05 23:03:30 +0800

    add compatible module to showcase multi jdk support

--- compatible/project-jdk21/src/main/java/com/yangbingdong/java/newfeat/introduction/LogMainJava21.java
+++ compatible/project-jdk21/src/main/java/com/yangbingdong/java/newfeat/introduction/LogMainJava21.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 /**
  * @author bingdong.yang@salesforce-china.com
  */
-public class LogMainJava8 {
+public class LogMainJava21 {
 
     public static void main(String[] args) {
         LogsUtil.log("Hello world.");
