61444fc36f0993645d22cd280f7db909ea90ed58
Bingdong Yang <bingdong.yang@salesforce-china.com>
2024-12-16 17:30:30 +0800

    upgrade 21 to 23

--- compatible/project-jdk23/src/main/java/com/yangbingdong/java/newfeat/introduction/Base64MainJava23.java
+++ compatible/project-jdk23/src/main/java/com/yangbingdong/java/newfeat/introduction/Base64MainJava23.java
@@ -3,7 +3,7 @@ package com.yangbingdong.java.newfeat.introduction;
 /**
  * @author yangbingdong1994@gmail.com
  */
-public class Base64MainJava21 {
+public class Base64MainJava23 {
 
     public static void main(String[] args) {
         String originalString = "Hello, Java!";
---
+++ compatible/project-jdk23/src/main/java/com/yangbingdong/java/newfeat/introduction/LogMainJava23.java
@@ -0,0 +1,8 @@
+import com.yangbingdong.java.newfeat.introduction.LogsUtil;
+
+/**
+ * @author bingdong.yang@salesforce-china.com
+ */
+void main() {
+    LogsUtil.log("Hello world.");
+}
